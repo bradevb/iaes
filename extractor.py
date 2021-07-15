08968fd3b6c165a40001f1851e92588e596ca8df
bradenBDev <70188874+bradenBDev@users.noreply.github.com>
2021-07-15 08:30:09 -0600

    Make base form extraction class

---
+++ extractor.py
@@ -0,0 +1,24 @@
+import cv2 as cv
+import numpy as np
+from utils import load_image, show_result
+
+
+class FormExtractor:
+    def __init__(self, preprocessors, output_process=False):
+        assert isinstance(preprocessors, list), "List of processors expected"
+        self._preprocessors = preprocessors
+        self.output_process = output_process
+
+    def __call__(self, img_or_path):
+        # Load image
+        self._image = load_image(img_or_path)
+
+        # Apply all preprocessors to the image
+        self._processed = self._image
+        for preprocessor in self._preprocessors:
+            self._processed = preprocessor(self._processed)
+
+        return self._extract_forms()
+
+    def _extract_forms(self):
+        pass
