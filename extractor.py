9ece76db1ac6815e38135704a55627a9e3711e0b
bradenBDev <70188874+bradenBDev@users.noreply.github.com>
2021-07-18 17:20:02 -0600

    Implement CellExtractor class for Captiva's form cells

--- extractor.py
+++ extractor.py
@@ -65,3 +65,58 @@ class FormExtractor(Extractor):
                     image_utils.show_result(form)
 
         return forms
+
+
+class CellExtractor(Extractor):
+    """
+    Class for extracting cells from a table.
+    Recommended preprocessing steps for Captiva's IAES form:
+    1. cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    2. cv.threshold(gray_scale_image, 200, 225, cv.THRESH_BINARY)
+    """
+
+    def __init__(self, preprocessors, line_width=1, line_min_width=20, output_process=False):
+        super().__init__(preprocessors, output_process)
+        self.line_width = line_width
+        self.line_min_width = line_min_width
+
+    def _extract(self):
+        kernel1 = np.ones((self.line_width, self.line_width), np.uint8)
+        kernel1h = np.ones((1, self.line_width), np.uint8)
+        kernel1v = np.ones((self.line_width, 1), np.uint8)
+        kernel6 = np.ones((self.line_min_width, self.line_min_width), np.uint8)
+        kernel6h = np.ones((1, self.line_min_width), np.uint8)
+        kernel6v = np.ones((self.line_min_width, 1), np.uint8)
+
+        # Bridge small gap in horizontal lines, erode everything else in horizontal direction
+        img_bin_h = cv.morphologyEx(~self._processed, cv.MORPH_CLOSE, kernel1h)
+        img_bin_h = cv.morphologyEx(img_bin_h, cv.MORPH_OPEN, kernel6h)
+        # Bridge small gap in vert lines, erode everything else in vertical direction
+        img_bin_v = cv.morphologyEx(~self._processed, cv.MORPH_CLOSE, kernel1v)
+        img_bin_v = cv.morphologyEx(img_bin_v, cv.MORPH_OPEN, kernel6v)
+        img_bin_final = self._fix_as_binary(self._fix_as_binary(img_bin_h) | self._fix_as_binary(img_bin_v))
+        # Dilate the final binary image a little bit to ensure all cells are connected
+        final_kernel = np.ones((5, 5), np.uint8)
+        img_bin_final = cv.dilate(img_bin_final, final_kernel, iterations=1)
+
+        ret, labels, stats, centroids = cv.connectedComponentsWithStats(~img_bin_final, connectivity=8, ltype=cv.CV_32S)
+
+        cells = []
+        for x, y, w, h, area in stats[2:]:
+            if area > 100:
+                cell = self._image[y:y + h, x:x + w]
+                cells.append(cell)
+
+        if self.output_process:
+            debug_image = self._image.copy()
+            for x, y, w, h, area in stats[2:]:
+                cv.rectangle(debug_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            show_result(debug_image)
+
+        return cells
+
+    @staticmethod
+    def _fix_as_binary(img):
+        img[img > 127] = 255
+        img[img < 127] = 0
+        return img
