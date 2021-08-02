13ac89b8e709dcb03329b6499bd08ab1a9485660
bradenBDev <70188874+bradenBDev@users.noreply.github.com>
2021-08-02 17:56:03 -0600

    Add docstring to validators package

--- validators/__init__.py
+++ validators/__init__.py
@@ -1,1 +1,15 @@
-from .dates import check_date_format
+"""
+Validators to use when validating cells.
+
+There are a few different types, and some of the modules are intended to be used
+internally by more concrete implementations of validators that are specifically for
+validating IAES cells. The internal modules are prefixed with an underscore.
+
+The concrete implementations take an entire DataFrame of all cell data that is found.
+They then extract only the information that they need to validate.
+For example, a date validator receives the whole DataFrame, then it isolates the
+DataFrame's to_date column, and finally performs all needed validations on said column.
+
+These modules and their members are subject to change as development continues
+and if a better method for validating is found.
+"""
