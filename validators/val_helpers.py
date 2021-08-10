426bd4a3d83292a77f6fbc694c64d4361242e447
bradenBDev <70188874+bradenBDev@users.noreply.github.com>
2021-08-09 20:57:32 -0600

    Change amount to float conversion's implementation (wow, can't believe I didn't think of this in the first place :)

--- validators/val_helpers.py
+++ validators/val_helpers.py
@@ -2,4 +2,4 @@ def amount_to_float(amount):
     """Inserts a decimal before the last two characters of amount and returns a float."""
     if amount is None:
         return None
-    return float(f'{amount[:-2]}.{amount[-2:]}')
+    return int(amount) / 100
