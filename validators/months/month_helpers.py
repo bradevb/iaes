826a8eba37f1b78618428e61c32db1c29368d0ab
bradenBDev <70188874+bradenBDev@users.noreply.github.com>
2021-08-09 19:28:32 -0600

    Refactor month_helpers.py

--- validators/months/month_helpers.py
+++ validators/months/month_helpers.py
@@ -5,22 +5,20 @@ def _amount_to_float(amount):
     return float(f'{amount[:-2]}.{amount[-2:]}')
 
 
-def _format_amount_cols(df):
-    """Returns a copy of df, with transactions formatted as floats."""
-    temp_df = df.copy()
-    temp_df['to_amount'] = temp_df['to_amount'].apply(lambda x: _amount_to_float(x))
-    temp_df['from_amount'] = temp_df['from_amount'].apply(lambda x: _amount_to_float(x))
-    temp_df.to_amount.fillna(value=0, inplace=True)
-    temp_df.from_amount.fillna(value=0, inplace=True)
-    return temp_df
+def _format_amount_cols(frame):
+    """Returns a copy of frame, with transactions formatted as floats."""
+    df = frame.copy()
+    df['to_amount'] = df['to_amount'].apply(lambda x: _amount_to_float(x))
+    df['from_amount'] = df['from_amount'].apply(lambda x: _amount_to_float(x))
+    df = df.fillna({'to_amount': 0, 'from_amount': 0})
+    return df
 
 
-def _calc_net(df, calc_cumnet=False):
-    """Calculates net gain or loss for months, and adds results to new column inplace.
-    If calc_cumnet is True, then also adds a cumulative net column."""
+def _calc_net(frame):
+    """Calculates net gain or loss for months, and returns a copy of frame with net columns."""
+    df = frame.copy()
     df['_net'] = df.apply(lambda x: x['to_amount'] - x['from_amount'], axis=1)
-    if calc_cumnet:
-        df['_cumnet'] = df['_net'].cumsum()
+    df['_cumnet'] = df['_net'].cumsum()
     return df
 
 
@@ -36,16 +34,16 @@ def _group_multi_payments(frame):
     return grouped
 
 
-def calc_balance(df, start_amount):
+def calc_balance(frame, start_amount):
     """Calculates balance for each month. Return slice of date and balance for each month."""
     if isinstance(start_amount, str):
         start_amount = _amount_to_float(start_amount)
 
-    temp_df = _format_amount_cols(df)
-    temp_df = temp_df.dropna(how='all')
-    temp_df = _group_multi_payments(temp_df)
+    df = frame.dropna(how='all')
+    df = _format_amount_cols(df)
+    df = _group_multi_payments(df)
+    df = _calc_net(df)
 
-    _calc_net(temp_df, calc_cumnet=True)
-    temp_df['balance'] = temp_df['_cumnet'] + start_amount  # Add cumulative net gain/loss to start_amount
+    df['balance'] = df['_cumnet'] + start_amount  # Add cumulative net gain/loss to start_amount
 
-    return temp_df.loc[:, ('to_date', 'balance')]
+    return df.loc[:, ('to_date', 'balance')]
