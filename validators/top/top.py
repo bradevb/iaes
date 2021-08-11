43a55ac7dbb10c370a708b2e7fcbdeb42569496a
bradenBDev <70188874+bradenBDev@users.noreply.github.com>
2021-08-10 20:30:21 -0600

    Add top validator that ensures all cells are present

--- validators/top/top.py
+++ validators/top/top.py
@@ -16,6 +16,13 @@ def ensure_total_amount(df):
         raise ValueError("Monthly PI and Escrow amounts don't add up to total monthly payment.")
 
 
+def ensure_all_cells_present(df):
+    cells = df.values
+    if not all(cells) or len(cells) < 7:
+        raise ValueError('A cell in the top form is missing. They must all be present.')
+
+
 TOP_VALIDATORS = [
     ensure_total_amount,
+    ensure_all_cells_present,
 ]
